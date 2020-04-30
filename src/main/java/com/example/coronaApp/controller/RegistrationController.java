496fc00b7a4269070e2ee4185ee2a54a81da8ef4
Matrixgek91 <jschouw1@gmail.com>
2020-04-30 11:17:30 +0200

    Cleanup

--- src/main/java/com/example/coronaApp/controller/RegistrationController.java
+++ src/main/java/com/example/coronaApp/controller/RegistrationController.java
@@ -4,11 +4,8 @@ package com.example.coronaApp.controller;
 import com.example.coronaApp.model.Registration;
 import com.example.coronaApp.service.RegistrationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.Optional;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
@@ -21,26 +18,7 @@ public class RegistrationController {
         return registrationService.save(registration);
     }
 
-    @PutMapping("/registration")
-    public Registration update(@RequestBody Registration registration) {
-        return registrationService.save(registration);
-    }
 
-    @ResponseStatus(value = HttpStatus.OK)
-    @DeleteMapping("/registration/{userId}")
-    public void delete(@PathVariable int userId) {
-        registrationService.deleteById(userId);
-    }
-
-    @GetMapping("/registration")
-    public List<Registration> findAll() {
-        return (List<Registration>)registrationService.findAll();
-    }
-
-    @GetMapping("/registration/{userId}")
-    public Optional<Registration> registrationById(@PathVariable int userId){
-        return registrationService.findById(userId);
-    }
 
 
 
